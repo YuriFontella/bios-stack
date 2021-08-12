f62fa6531f54ae680b6b5cc3273790c7829d1a98
Yuri Fontella <yurifc4@gmail.com>
2021-08-12 14:58:16 -0300

    next 11.1

--- src/components/libs/toast/index.js
+++ src/components/libs/toast/index.js
@@ -4,7 +4,7 @@ const Toast = ({ message }) => {
 
   return (
     <div className="fixed w-full top-0 z-50">
-      <div className="flex justify-center items-center z-50 space-x-3 text-white text-xs p-3 shadow-xl bg-blue-500">
+      <div className="flex justify-center items-center space-x-3 text-white text-xs p-3 shadow-xl bg-blue-500">
         <span>
           <SpeakerphoneIcon className="h-5 w-5" />
         </span>
